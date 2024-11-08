8d59767945936cea9a6a6db318b49939579fed6c
emanu🪬 <emanuellesilvestre06@gmail.com>
2024-11-08 18:55:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cinema e História',
+    'Quando o cinema começou?',
+    'O cinema começou no seculo XIX'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Cinema e História',
+    'Qual era a principal caracteristica dos filmes antigos?',
+    'A principal caracteristica era a coloração dos filmes'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz chorar em Inglês?',
+    'Oi em ingles é CRY (quay)'
+)
